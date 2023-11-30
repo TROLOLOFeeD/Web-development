56565c39503ada8f4f3ee98bf55d482ea19b9606
Балдан <tsybenov.bs@yandex.ru>
2023-11-30 22:14:34 +0800

    Редакт клиент и докер-композ

--- client/client.py
+++ client/client.py
@@ -1,1 +1,13 @@
+#!/usr/bin/end python3
 
+from codecs import EncodedFile
+import urllib.request
+
+fp = urllib.request.urlopen("http://localhost:1234/")
+
+encodedContent = fp.read()
+decodedContent = encodedContent.decode("utf8")
+
+print(decodedContent)
+
+fp.close()
