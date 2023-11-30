3afeaac23ad011a0ae9d5afe7146e6afe183cac1
Балдан <tsybenov.bs@yandex.ru>
2023-11-30 21:39:06 +0800

    Редакт папки сервер

--- server/server.py
+++ server/server.py
@@ -1,1 +1,11 @@
+#!/usr/bin/env python3
 
+import http.server
+import socketserver
+
+handler = http.server.SimpleHTTPRequestHandler
+#—ервер на порте 1234
+
+with socketserver.TCPServer(("", 1234), handler) as httpd:
+    #—ервер будет выполн€тьс€ посто€нно, ожида€ запросов от клиента
+    httpd.serve_forever()
